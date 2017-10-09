77c588ce04d61e2b8477d3000f7332e464ad116e
Rick <kylin792884522@outlook.com>
2017-10-09 09:51:19 +0800

    complete

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sourceId = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.msgBox = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.URL = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.run = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sourceId
+            // 
+            this.sourceId.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.sourceId.Location = new System.Drawing.Point(0, 41);
+            this.sourceId.Margin = new System.Windows.Forms.Padding(4);
+            this.sourceId.Name = "sourceId";
+            this.sourceId.Size = new System.Drawing.Size(169, 28);
+            this.sourceId.TabIndex = 3;
+            this.sourceId.TextChanged += new System.EventHandler(this.sourceId_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip2.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip2_Opening);
+            // 
+            // msgBox
+            // 
+            this.msgBox.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.msgBox.Location = new System.Drawing.Point(0, 84);
+            this.msgBox.Margin = new System.Windows.Forms.Padding(4);
+            this.msgBox.Name = "msgBox";
+            this.msgBox.Size = new System.Drawing.Size(316, 180);
+            this.msgBox.TabIndex = 4;
+            this.msgBox.Text = "";
+            this.msgBox.TextChanged += new System.EventHandler(this.msgBox_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(270, 7);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(356, 257);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.msgBox);
+            this.groupBox1.Controls.Add(this.URL);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.run);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.sourceId);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(897, 333);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Norway Forum Downloader";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 41);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "TMonkey";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // URL
+            // 
+            this.URL.Location = new System.Drawing.Point(0, 272);
+            this.URL.Margin = new System.Windows.Forms.Padding(4);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(626, 31);
+            this.URL.TabIndex = 7;
+            this.URL.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton1.Location = new System.Drawing.Point(337, 7);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(203, 28);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Working at Norway";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // run
+            // 
+            this.run.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.run.Location = new System.Drawing.Point(324, 233);
+            this.run.Margin = new System.Windows.Forms.Padding(4);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(137, 31);
+            this.run.TabIndex = 7;
+            this.run.Text = "run to test";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(489, 233);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "test result";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(655, 322);
+            this.Controls.Add(this.groupBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "    舰队指挥面板";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox sourceId;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.RichTextBox msgBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox URL;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Collections.Specialized;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.sourceId.KeyDown += new KeyEventHandler(sourceId_KeyDown);
+        }
+        void config(string _FileName)
+        {
+
+            if (File.Exists(_FileName))
+            {
+                StreamReader sr = new StreamReader(_FileName, System.Text.Encoding.Default);
+                string str = sr.ReadToEnd();
+                sr.Close();
+                str = Regex.Replace(str, @"<UrlTransformation>(\d+)</UrlTransformation>", m =>
+                {
+                    return "<UrlTransformation>5</UrlTransformation>";
+                });
+                str = Regex.Replace(str, @"<ThreadTransformation>(\d+)</ThreadTransformation>", m =>
+                {
+                    return "<ThreadTransformation>6</ThreadTransformation>";
+                });
+                File.WriteAllText(_FileName, str, System.Text.Encoding.Default);
+            }
+            else msgBox.Text="error";
+        }
+        void config2(string _FileName,string id)
+        {
+            if (File.Exists(_FileName))
+            {
+            //    Encoding winLatinCodePage = Encoding.GetEncoding(1252);
+
+            //    Byte[] bytes = Encoding.Convert(Encoding.UTF8, winLatinCodePage, Encoding.UTF8.GetBytes(s));
+                StreamReader sr = new StreamReader(_FileName, System.Text.Encoding.Default);
+                string str = sr.ReadToEnd();
+                sr.Close();
+                str = Regex.Replace(str,@"(\w+.\w+)-url.xq</UrlTransformation>", m =>
+                {
+                    return id+"-url.xq</UrlTransformation>";
+                });
+               
+                File.WriteAllText(_FileName, str, System.Text.Encoding.Default);
+            }
+            else msgBox.Text = "error";
+        }
+        public class CookieAwareWebClient : WebClient
+        {
+            private CookieContainer cookie = new CookieContainer();
+
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                WebRequest request = base.GetWebRequest(address);
+                if (request is HttpWebRequest)
+                {
+                    (request as HttpWebRequest).CookieContainer = cookie;
+                }
+                return request;
+            }
+        }
+        string connectingName;
+        public void Navigate(string id)
+        {
+            try
+            {
+
+                    CookieAwareWebClient wc = new CookieAwareWebClient();
+                    string archerCase = "http://115.159.41.49:10000";
+                    if (connectingName == "Norway")
+                    {
+                        archerCase = "http://p1.integrasco.com:10000";
+                    }
+                string archerCase2 = archerCase + "/api/source/submit/file/?source_id=" + id + "&filename=";
+                wc.BaseAddress = archerCase;
+                string[] Url = new string[3];
+                    Url[0] = archerCase2 +"url_transformation";
+                    Url[1] = archerCase2 + "thread_transformation";
+                    Url[2] = archerCase2 + "config";
+                var loginData = new NameValueCollection();
+                loginData.Add("username", "kylin792884522@gmail.com");
+                loginData.Add("password", "123456");
+                loginData.Add("login", "Login");
+                wc.UploadValues("", "POST", loginData);
+                string path = @"D:\Dian\Norway\Download\" + id;
+                Directory.CreateDirectory(path);
+                string newPath = @"D:\Dian\Norway\Download\" + id;
+                for (var i = 0; i < 3; i++)
+                    {
+                        string url = @Url[i];
+                        switch (i)
+                        {
+                            case 0:
+                                wc.DownloadFile(url, newPath + "\\" + id + "-url.xq");
+                                break;
+                            case 1:
+                                wc.DownloadFile(url, newPath + "\\" + id + "-thread.xq");
+                                break;
+                            case 2:
+                                wc.DownloadFile(url, newPath + "\\" + "webForumConfiguration.xml");
+                                config(newPath + "\\" + "webForumConfiguration.xml");
+                                File.Copy(@"D:\Dian\Norway\SubSourceCrawlerConfig.xml", newPath + "\\" + "SubSourceCrawlerConfig.xml", true);
+                                config2(newPath + "\\" + "SubSourceCrawlerConfig.xml", id);
+                                File.Copy(@"D:\Dian\Norway\finished.xml", newPath + "\\" + "finished.xml");
+                                break;
+                             default:
+                                break;
+                        }
+                }
+                System.Diagnostics.Process.Start(newPath);
+                msgBox.Text = msgBox.Text+"报告司令大人， 星际#" + id + "舰已经装载完毕【敬礼\n";
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    //  pingDian();
+                    msgBox.Text += e.ToString();
+                }
+                catch (Exception c)
+                {
+                    msgBox.Text = e.ToString()+c.ToString();
+                } 
+            }
+        }
+
+        private void sourceId_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                NewMethod();
+            }
+        }
+
+        private void NewMethod()
+        {
+            Navigate(sourceId.Text);
+        }
+
+        private void pingDian()
+        {
+            try
+            {
+                Process p = Process.Start(@"D:\Dian\ingentia-run\ingentia-run\ping.bat");
+                msgBox.Text = p.ToString();
+             
+            }
+            catch (Exception e)
+            {
+                msgBox.Text = msgBox.Text + e.ToString();
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip2_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void sourceId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void msgBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            msgBox.Text = msgBox.Text + "\n好好做source啊骚年！";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            connectingName = "Norway";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string str =" \""+URL.Text+"\"";
+            download(str);
+            System.Diagnostics.Process.Start(@"D:\Dian\Norway\download.xml");
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+               
+            }
+        }
+        public void download(string url)
+        {
+            try
+            {
+                Process p = Process.Start(@"D:\Dian\ingentia-run\ingentia-run\download.bat", url);
+                msgBox.Text = p.ToString();
+               // msgBox.Text = "3";
+            }
+            catch (Exception e)
+            {
+                msgBox.Text = msgBox.Text + e.ToString();
+            }
+        }
+        private void run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process p = Process.Start(@"D:\Dian\forumtest\bin\run.bat");
+               
+            }
+            catch (Exception c)
+            {
+                msgBox.Text = msgBox.Text + e.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process p = Process.Start(@"D:\Dian\Tmonkey\Tmonkey\dm_tmonkey_sandbox\tmonkey.bat");
+            }
+            catch (Exception c)
+            {
+                msgBox.Text = msgBox.Text + e.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string path = @"D:\Dian\forumtest\temp\" + sourceId.Text+"\\";
+            string errorfile = "errors.log";
+            string transformed =  "transformed.log";
+           
+           
+                if (FindFile(path, errorfile))
+                {
+                    MessageBox.Show("存在错误");
+                }
+                else if (FindFile(path, transformed))
+                {
+                    MessageBox.Show("存在transformation");
+                }
+                else
+                {
+                    msgBox.Text = msgBox.Text + "还没有结果" + sourceId.Text;
+                }
+        }
+              /// <summary>
+        　　/// C#查找指定文件
+        　　/// </summary>
+        　　/// <param name="filePath">文件夹路径</param>
+        　　/// <param name="fileName">查找文件名</param>
+        　　/// <returns>找到返回真，否则返回假</returns>
+        private bool FindFile(string filePath, string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)) return false;
+
+            DirectoryInfo di = new DirectoryInfo(filePath);
+            DirectoryInfo[] arrDir = di.GetDirectories();
+
+            foreach (DirectoryInfo dir in arrDir)
+            {
+                if (FindFile(di + dir.ToString() + "\\", fileName))
+                    return true;
+            }
+
+            foreach (FileInfo fi in di.GetFiles("*.*"))
+            {
+                if (fi.Name == fileName)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
